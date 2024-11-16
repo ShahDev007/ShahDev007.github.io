f7ad98d9b55c1cfb9ddf22a3118ed2e5a12ac550
ShahDev007 <devshahtech@gmail.com>
2024-11-16 10:14:08 -0800

    Chngaed config file

--- next.config.js
+++ next.config.js
@@ -5,7 +5,30 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      'media.dev.to', 
+      'avatars.githubusercontent.com', 
+      'github-readme-stats.vercel.app', 
+      'github-readme-streak-stats.herokuapp.com', 
+      'stardev.io', 
+      'github-profile-summary-cards.vercel.app', 
+      'github-profile-trophy.vercel.app'
+    ],
     dangerouslyAllowSVG: true,
+    unoptimized: true,  // Add this line
+    remotePatterns: [   // Add this section
+      {
+        protocol: 'https',
+        hostname: '**.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.herokuapp.com',
+      }
+    ],
   }
 }
