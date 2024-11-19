2e92e350310c4620cc794ccfa759eb2b4aac88b9
ShahDev007 <devshahtech@gmail.com>
2024-11-19 08:55:17 -0800

    Arrow length improved

--- app/components/projects/index.jsx
+++ app/components/projects/index.jsx
@@ -24,7 +24,7 @@ const Projects = ({ projects, profile }) => {
         <Link href={profile.html_url} target='_blank' className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
           <button className="px-3 text-xs md:px-4 py-2 md:py-2.5 bg-[#0d1224] rounded-full border-none text-center font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
             <span>View All Project</span>
-            <FaLongArrowAltRight size={16} />
+            <FaLongArrowAltRight size={17} />
           </button>
         </Link>
       </div>
